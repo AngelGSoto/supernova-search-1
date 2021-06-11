3659e394ea49725738f9eae7506238358d69ceec
Julia <julia.mello.oliveira@usp.br>
2021-06-11 14:53:08 -0300

    R-band and new file names

--- splus-cuts.py
+++ splus-cuts.py
@@ -7,6 +7,4 @@ df = pd.read_csv('selected-gals.csv')
 
 for key, value in df.iterrows():
     hdu = conn.get_cut(value.RA, value.DEC, 128, 'R')
-    print(hdu)
-    print(value.RA, value.DEC)
-    hdu.writeto(f'{value.ID}.fits')
+    hdu.writeto('stamps-splus/%.6f_%.6f.fz' % (value.RA, value.DEC))
