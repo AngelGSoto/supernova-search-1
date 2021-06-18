ec6f40e11241a2fda2dcf2877b0c6b3ac96306d3
Julia <julia.mello.oliveira@usp.br>
2021-06-18 14:58:07 -0300

    fixing gaussian conv

--- codes/gaussian.py
+++ codes/gaussian.py
@@ -7,16 +7,17 @@ from astropy.wcs import WCS
 from matplotlib.colors import LogNorm
 
 fig = plt.figure()
-hdu = fits.open('frame-r-006372-5-0073.fits')
+#hdu = fits.open('frame-r-006372-5-0073.fits')
+hdu = fits.open('iDR3.STRIPE82-0001.000260-crop.fits')
 data = hdu[0].data
 #plt.gray()  # show the filtered result in grayscale
 ax1 = fig.add_subplot(121)  # left side
 ax2 = fig.add_subplot(122)  # right side
 ascent = data
-result = gaussian_filter(data, sigma=5)
-print('antes: ', ascent)
-print( )
-print('depois: ', result)
-ax1.imshow(ascent, origin='lower', norm=LogNorm())
-ax2.imshow(result, vmin=2.e3, vmax=3.e3, interpolation='nearest', origin='lower', norm=LogNorm())
+result = gaussian_filter(data, sigma=1)
+#print('antes: ', ascent)
+#print( )
+#print('depois: ', result)
+ax1.imshow(ascent, vmin=0, vmax=3)
+ax2.imshow(result, vmin=0, vmax=3)
 plt.show()
